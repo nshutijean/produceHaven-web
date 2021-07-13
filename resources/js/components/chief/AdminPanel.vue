544ac6c417951ab0a72d61ffdb221f9d598b0959
nshutijean <j.nshuti73@gmail.com>
2021-07-13 18:55:13 +0200

    remove search bar

--- resources/js/components/chief/AdminPanel.vue
+++ resources/js/components/chief/AdminPanel.vue
@@ -29,19 +29,6 @@
                         </span>
                     </div>
                 </div>
-                <!-- sidebar-search  -->
-                <div class="sidebar-item sidebar-search">
-                    <div>
-                        <div class="input-group">
-                            <input type="text" class="form-control search-menu" placeholder="Search...">
-                            <div class="input-group-append">
-                                <span class="input-group-text">
-                                    <i class="fa fa-search" aria-hidden="true"></i>
-                                </span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <!-- sidebar-menu  -->
                 <div class=" sidebar-item sidebar-menu">
                     <ul>
