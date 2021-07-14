e02f81380b0257b62a395d6a1230e5fd10e31b90
nshutijean <j.nshuti73@gmail.com>
2021-07-14 23:34:59 +0200

    Fix: Buyers who made orders in a specific store

--- resources/js/components/admin/Users.vue
+++ resources/js/components/admin/Users.vue
@@ -1,37 +1,80 @@
 <template>
+    <div class="col-lg-12 col-md-8">
     <div>
-        <table class="table table-responsive table-striped">
-            <thead>
-                <tr>
-                    <td></td>
-                    <td>Name</td>
-                    <td>Email</td>
-                    <td>Joined</td>
-                    <td>Total Orders</td> 
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="(user, index) in users" v-bind:key="index">
-                    <td>{{index+1}}</td>
-                    <td>{{user.name}}</td>
-                    <td>{{user.email}}</td>
-                    <td>{{user.created_at}}</td>
-                    <td>{{user.orders.length}}</td>
-                </tr>
-            </tbody>
-        </table>
+      <div
+        class="float-left"
+        style="font-weight: 600; font-size: 1.125rem; line-height: 1.75rem"
+      >
+        <p>Users who made orders in this store</p>
+      </div>
     </div>
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead class="thead-inverse">
+            <tr style="font-weight: 600;">
+                <td>#</td>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Phonenumber</td>
+                <td>Joined</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(user, index) in users" :key="index">
+                <td>{{index+1}}</td>
+                <td>{{user.name}}</td>
+                <td>{{user.email}}</td>
+                <td>{{user.phoneNumber}}</td>
+                <td>{{user.created_at}}</td>
+                <!-- <td>{{user.orders.length}}</td> -->
+            </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-            users: []
+            users: [],
+            vendorUser: null,
+            order: null,
+            newUsers: [],
+            orders: [],
+            newOrders: []
         }
     },
     beforeMount() {
-        axios.get('/api/users/').then(response => this.users = response.data)
+        this.vendorUser = JSON.parse(localStorage.getItem('bigStore.user'));
+        this.getUsersWithOrders()
+    },
+    methods: {
+        getUsersWithOrders() {
+            axios.get('/api/orders/')
+            .then(
+                response => {
+                    this.orders = response.data
+                    this.orders.forEach(order => {
+                        // orders from this specific vendor
+                        if(this.vendorUser.id == order.product.user_id) {
+                            axios.get(`/api/users/${order.user_id}`)
+                            .then((response) => {
+                                //insert response.data into this.users array
+                                this.users.push(response.data)
+                                console.log(this.users)
+                            }).catch((err) => {
+                                console.error(err)
+                            });
+                        } 
+                    });
+                }
+            )
+            .catch(err => {
+                console.log(err)
+            })
+        }
     },
 }
 </script>
